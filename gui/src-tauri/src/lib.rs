3b65583e238c243374261c4ff7e1762175c4ce2e
DCP Doctor <dcpdoctor@proton.me>
2026-08-16 10:49:04 -0400

    remember window state across restarts

--- gui/src-tauri/src/lib.rs
+++ gui/src-tauri/src/lib.rs
@@ -93,6 +93,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
